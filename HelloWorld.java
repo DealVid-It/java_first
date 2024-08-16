9ad33e86172845d3ce1d5f960e5bfc4ff07c8c11
dealVid_it <theviditnayal@gmail.com>
2024-08-16 11:59:20 +0530

    first commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
      }   
     public static void main(String[] args) {
         System.out.println("Hello Java World");
+System.out.println("This is my first program");
          method();
     }
 }
